1f045f97ff3244e7fff808508ed8cae7ace82695
Paul Gaston <paul.gaston@sfr.fr>
2021-04-04 18:46:11 +0200

    [INIT] Add empty extension

---
+++ README.md
@@ -0,0 +1,3 @@
+## FPL History
+
+The purpose of this extension is to allow users of Flashpoint of tracking items that they play in it
---
+++ src/ext.ts
@@ -0,0 +1,5 @@
+import * as flashpoint from 'flashpoint-launcher';
+
+export function activate(context: flashpoint.ExtensionContext) {
+    flashpoint.log.info('History extension loaded');
+}
